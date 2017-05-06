faf67d0a0c40a5643a94d5ef2c9d8ad1c66d78ee
mmassaroni <miguelmassaroni@gmail.com>
2017-05-06 12:14:16 -0300

    menu
    cambio de fuente
    arregle el error de la cruz
    agrege logo
    otros.
    
    FALTA:
    correguir botones del topnav
    arrglar los botones sociales
    Agregar el pop de bootstrap para el usu

--- Diseno/Sitio/js/slideRigth.js
+++ Diseno/Sitio/js/slideRigth.js
@@ -2,27 +2,32 @@ $(document).ready(function($){
     $(".cruz").click(function(){
         var div_left = $("#mySidenav").position();
         if (div_left.left == -250) {
+            document.getElementById("mySidenav").style.opacity = "1";
             document.getElementById("mySidenav").style.left = "0";
             document.getElementById("out").style.opacity = "1";
+            $('.barras').addClass('change');
         } else {
+            document.getElementById("mySidenav").style.opacity = "0";
             document.getElementById("mySidenav").style.left = "-250px";
             document.getElementById("out").style.opacity = "0";
+            $('.barras').removeClass('change');
         }
     });
 
     $("#out").click(function(){
         var div_left = $("#mySidenav").position();
-        if (div_left.left == 0) {
+        if (div_left.left != -250) {
             $('.barras').removeClass('change');
         }
     });
 });  
 
 function closeNav() {
+    document.getElementById("mySidenav").style.opacity = "0";
     document.getElementById("mySidenav").style.left = "-250px";
     document.getElementById("out").style.opacity = "0";
 }
 
-function tranX(x) {
-    x.classList.toggle("change");
-}
+// function tranX(x) {
+//     x.classList.toggle("change");
+// }
