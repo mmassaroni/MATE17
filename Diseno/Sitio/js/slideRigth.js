855038d9ef6b77ecbfe1277048efc1e2231e93c4
mmassaroni <miguelmassaroni@gmail.com>
2017-06-05 20:30:26 -0300

    emprolijamiento

--- Diseno/Sitio/js/slideRigth.js
+++ Diseno/Sitio/js/slideRigth.js
@@ -5,19 +5,23 @@ $(document).ready(function($){
             document.getElementById("mySidenav").style.opacity = "1";
             document.getElementById("mySidenav").style.left = "0";
             document.getElementById("out").style.opacity = "1";
+            document.getElementById("out").style.zIndex = "899";
             $('.barras').addClass('change');
         } else {
             document.getElementById("mySidenav").style.opacity = "0";
             document.getElementById("mySidenav").style.left = "-250px";
             document.getElementById("out").style.opacity = "0";
+            document.getElementById("out").style.zIndex = "0";
             $('.barras').removeClass('change');
         }
     });
 
     $("#out").click(function(){
         var div_left = $("#mySidenav").position();
+        document.getElementById("out").style.zIndex = "0";
         if (div_left.left != -250) {
             $('.barras').removeClass('change');
+
         }
     });
 });  
@@ -27,7 +31,3 @@ function closeNav() {
     document.getElementById("mySidenav").style.left = "-250px";
     document.getElementById("out").style.opacity = "0";
 }
-
-// function tranX(x) {
-//     x.classList.toggle("change");
-// }
