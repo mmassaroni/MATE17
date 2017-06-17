1a6a2c046301ee05f6e04e064681d046863e6368
mmassaroni <miguelmassaroni@gmail.com>
2017-06-16 22:21:18 -0300

    irrelevante

--- Diseno/Sitio/js/menuScroll.js
+++ Diseno/Sitio/js/menuScroll.js
@@ -1,8 +1,8 @@
 $(window).scroll(function(){
     window_y = $(window).scrollTop();
     if (window_y > 50) {
-    	document.getElementById("menu").style.background = "#333";
+    	document.getElementById("menuContainer").style.background = "#222";
     } else {
-    	document.getElementById("menu").style.background = "rgba(0,0,0,0.8)";
+    	document.getElementById("menuContainer").style.background = "rgba(0,0,0,0.8)";
     }
 });
